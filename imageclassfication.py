e9002e551ed2c4b472f75e9546463f3f9fa14c97
fascinet <mohit26012000@gmail.com>
2019-03-07 20:10:27 +0530

    Add files via upload

---
+++ imageclassfication.py
@@ -0,0 +1,43 @@
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+classifier =Sequential()
+classifier.add(Convolution2D(32,3,3,input_shape=(64,64,3),activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2,2)))
+classifier.add(Flatten())
+classifier.add(Dense(units=128,activation='relu'))
+classifier.add(Dense(units=1,activation='sigmoid'))
+classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+
+##data preprocessing
+from keras.preprocessing.image import ImageDataGenerator
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_set = train_datagen.flow_from_directory(
+        'dataset/training_set',
+        target_size=(64, 64),
+        batch_size=32,
+        class_mode='binary')
+
+test_set = test_datagen.flow_from_directory(
+        'dataset/test_set',
+        target_size=(64, 64),
+        batch_size=32,
+        class_mode='binary')
+
+classifier.fit_generator(
+        train_set,
+        steps_per_epoch=8000,
+        epochs=25, 
+        validation_data=test_set,
+        validation_steps=800)
